fb6d0657fbf56f0d044110564894a2667e2befa6
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-05-08 16:58:23 +0100

    Basic Props

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,11 +1,37 @@
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 import "./App.css";
 
 function App() {
+	const personName = {
+		first: "Ivanka",
+		last: "Lopes",
+	};
+
+	const nameList = [
+		{
+			first: "Bruce",
+			last: "Wayne",
+		},
+		{
+			first: "Clark",
+			last: "Kent",
+		},
+		{
+			first: "Princess",
+			last: "Diana",
+		},
+	];
+
 	return (
 		<>
 			<div className='App'>
-				<Greet name='Ivanka'></Greet>
+				<Greet name='Ivanka' isLoggedIn={false} messageCount={20}></Greet>
+				<Person name={personName}></Person>
+
+				<h3 style={{ marginTop: "100px" }}>NameList:</h3>
+				<PersonList names={nameList}></PersonList>
 			</div>
 		</>
 	);
--- my-react-ts-app/src/components/Greet.tsx
+++ my-react-ts-app/src/components/Greet.tsx
@@ -1,11 +1,19 @@
 type GreetProps = {
 	name: string;
+	messageCount: number;
+	isLoggedIn: boolean;
 };
 
 export const Greet = (props: GreetProps) => {
 	return (
 		<div>
-			<h2>Welcome {props.name}, you have 10 unread messages</h2>
+			{props.isLoggedIn === true ? (
+				<h2>
+					Welcome {props.name}, you have {props.messageCount} unread messages
+				</h2>
+			) : (
+				<h2>please, login</h2>
+			)}
 		</div>
 	);
 };
