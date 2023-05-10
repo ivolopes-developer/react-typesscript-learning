8197fea2f7ec633f5900dbf3bee96e817a8ace0b
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-05-10 17:30:17 +0100

    Event Props

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -4,7 +4,9 @@ import { PersonList } from "./components/PersonList";
 import Status from "./components/Status";
 import { Heading } from "./components/Heading";
 import { Oscar } from "./components/Oscar";
+import { Button } from "./components/Button";
 import "./App.css";
+import { Input } from "./components/input";
 
 function App() {
 	const personName = {
@@ -30,17 +32,35 @@ function App() {
 	return (
 		<>
 			<div className='App'>
-				<Greet name='Ivanka' isLoggedIn={false} messageCount={20}></Greet>
+				<Greet name='Ivanka' isLoggedIn={true} messageCount={20}></Greet>
 				<Person name={personName}></Person>
-				<h3 className='videoTopic'>NameList:</h3>
+
+				<h3 className='videoTopic'>
+					<hr />
+					NameList:
+				</h3>
 				<PersonList names={nameList}></PersonList>
 
-				<h3 className='videoTopic'>Advanced props</h3>
+				<h3 className='videoTopic'>
+					<hr />
+					Advanced props
+				</h3>
 				<Status status='error'></Status>
 				<Oscar>
 					<Heading>Oscar goes to Dicaprio</Heading>
 				</Oscar>
 				<Greet name='Ivanka' isLoggedIn={false}></Greet>
+
+				<h3 className='videoTopic'>
+					<hr />
+					Event Props
+				</h3>
+				<Button
+					handleClick={(event, id) => {
+						console.log("Button clicked", event, id);
+					}}
+				></Button>
+				<Input value='' handleChange={(event) => console.log(event)}></Input>
 			</div>
 		</>
 	);
